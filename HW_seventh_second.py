4098cb852bbf395a571a40d17f3d93bf8b737fb0
svitlana.seleznova <seleznov@fuib.com>
2026-01-18 19:09:07 +0200

    Initial commit

---
+++ HW_seventh_second.py
@@ -0,0 +1,13 @@
+#text always started with a Capital letter and ended with a period.
+
+def correct_sentence(text):
+    text = text[0].upper() + text[1:]
+    if not text[-1] == '.':
+        text += '.'
+    return text
+
+print(correct_sentence("greetings, friends") ) #== "Greetings, friends.", 'Test1'
+print(correct_sentence("hello"))# == "Hello.", 'Test2'
+print(correct_sentence("Greetings. Friends"))# == "Greetings. Friends.", 'Test3'
+print(correct_sentence("Greetings, friends.") )#== "Greetings, friends.", 'Test4'
+print( correct_sentence("greetings, friends."))# == "Greetings, friends.", 'Test5'
