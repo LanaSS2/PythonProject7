999231d7408f88ada941c1965d12fa4c8850470d
svitlana.seleznova <seleznov@fuib.com>
2026-01-18 19:57:58 +0200

    7_3

---
+++ HW_seventh_third.py
@@ -0,0 +1,15 @@
+#find the index of the second entry of the second string in the first string.
+
+def second_index(text, some_str):
+    first = text.find(some_str)
+    if first == -1:
+        return None
+
+    second = text.find(some_str, first + len(some_str))
+    return None if second == -1 else second
+
+
+print(second_index("sims", "s"))# == 3, 'Test1'
+print(second_index("find the river", "e"))# == 12, 'Test2'
+print(second_index("hi", "h"))#  is None, 'Test3'
+print(second_index("Hello, hello", "lo"))#  == 10, 'Test4'
